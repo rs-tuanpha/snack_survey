b7ee34b916cd32f29153d04c82ef25309c4100fa
Nguyễn Thế An <annt1@runsystem.net>
2023-06-09 15:32:42 +0700

    Add file

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
-import './assets/styles/global.scss'
+//import './assets/styles/global.scss'
 
 const app = createApp(App)
 
