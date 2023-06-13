074fb4a12e19367814cab4f0ec2935eb8c360f94
Eric Nguyen <11a1eric3@gmail.com>
2023-06-13 16:58:59 +0700

    Fix: fix bug cannot get accounts and topics list in home page

--- src/stores/topic.ts
+++ src/stores/topic.ts
@@ -1,23 +1,25 @@
 import { defineStore } from 'pinia'
 import { useFirestore, useCollection } from 'vuefire'
 import { collection } from 'firebase/firestore'
+import { reactive } from 'vue'
+import type { ITopic } from '@/core/interfaces/model/topic'
+
 const db = useFirestore()
 export const useTopicStore = defineStore({
   id: 'topic',
   state: () => ({
-    openTopicList: {}
+    openTopicList: reactive<ITopic[]>([])
   }),
   getters: {
-    openList: () => {
-      return useCollection(collection(db, 'topics'));
-    },
+    openList: (state) => state.openTopicList.filter((topic) => topic.status === 'open'),
     accountList: () => {
       return useCollection(collection(db, 'accounts'))
     }
   },
   actions: {
     getOpenTopicList() {
-      this.openTopicList = useCollection(collection(db, 'topics'));
+      const res = useCollection<ITopic>(collection(db, 'topics'))
+      this.openTopicList = res.value
     }
   }
 })
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,32 +1,28 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import { useTopicStore } from '@/stores/topic'
-  import Cookies from 'js-cookie'
+import { computed, ref } from 'vue'
+import { useTopicStore } from '@/stores/topic'
+import Cookies from 'js-cookie'
 
-  const show = ref(true)
-  const accounts = useTopicStore().accountList;
-  const topics = ref(useTopicStore().openTopicList);
-  const account = ref(null);
+const show = ref(true)
+const topicStore = useTopicStore()
+const accounts = useTopicStore().accountList
+const topics = computed(() => topicStore.openTopicList)
+const account = ref(null)
 
-  const login = () => {
-    show.value = false;
-    useTopicStore().getOpenTopicList();
-    topics.value = useTopicStore().openTopicList;
-    Cookies.set('account_info', account.value, { expires: 7 });
-  }
+const login = () => {
+  show.value = false
+  topicStore.getOpenTopicList()
+  Cookies.set('account_info', account.value, { expires: 7 })
+}
 </script>
 
 <template>
-    <div class="py-4">
-      <v-container class="">
-        <v-row justify="center">
-            <v-col cols="6">
-            <v-card
-            class="mx-auto pa-4 pb-4"
-            max-width="448"
-            rounded="md"
-            v-if="show">
-              <v-autocomplete
+  <div class="py-4">
+    <v-container class="">
+      <v-row justify="center">
+        <v-col cols="6">
+          <v-card class="mx-auto pa-4 pb-4" max-width="448" rounded="md" v-if="show">
+            <v-autocomplete
               label="Chọn tài khoản"
               :items="accounts"
               item-title="username"
@@ -35,7 +31,7 @@
             >
             </v-autocomplete>
             <v-btn class="bg-blue-darken-2 float-right" @click="login">Đăng nhập</v-btn>
-            </v-card>	
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -44,16 +40,24 @@
       <v-row justify="center">
         <v-col cols="6">
           <v-row justify="center">
-          <v-col v-for="{id, name, description} in topics" :key="id" cols="12">
-          <v-hover v-slot="{ isHovering, props }">
-            <v-card :title="name" :text="description" variant="outlined"  :elevation="isHovering ? 12 : 2" v-bind="props" :class="isHovering ? 'bg-indigo-lighten-2' : ''"
-              target="_blank" :href="'vote-topic/'+ id">
-            </v-card>
-          </v-hover>
-          </v-col>
+            <v-col v-for="{ id, name, description } in topics" :key="id" cols="12">
+              <v-hover v-slot="{ isHovering, props }">
+                <v-card
+                  :title="name"
+                  :text="description"
+                  variant="outlined"
+                  :elevation="isHovering ? 12 : 2"
+                  v-bind="props"
+                  :class="isHovering ? 'bg-indigo-lighten-2' : ''"
+                  target="_blank"
+                  :href="'vote-topic/' + id"
+                >
+                </v-card>
+              </v-hover>
+            </v-col>
           </v-row>
         </v-col>
       </v-row>
     </v-container>
-    </div>
+  </div>
 </template>
