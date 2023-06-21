16b25685c5b34ce0256bec03f8d478e3f7a86240
rs-tuanpha <76983281+rs-tuanpha@users.noreply.github.com>
2023-06-21 13:21:17 +0700

    update route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import checkAuth from './auth.router'
 
 const routes = [
@@ -27,7 +27,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: routes,
   // Always scroll to top
   scrollBehavior() {
