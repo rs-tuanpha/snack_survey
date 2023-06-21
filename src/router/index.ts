b32fcb9d80e570ff77ea6c0ac649976192c68498
rs-tuanpha <76983281+rs-tuanpha@users.noreply.github.com>
2023-06-21 12:59:56 +0700

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -27,7 +27,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes,
   // Always scroll to top
   scrollBehavior() {
